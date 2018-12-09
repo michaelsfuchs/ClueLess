d333f68492a0cd4720ad60cc96505b0991a3683e
Alisha <ahunt221b@gmail.com>
2018-12-08 23:45:12 -0500

    changed array size for initCards

--- UIfiles/src/clueLess/ClientCon.java
+++ UIfiles/src/clueLess/ClientCon.java
@@ -185,8 +185,9 @@ public class ClientCon extends Thread
             //Inform that player of cards that can disprove a suggestion
             //New function
             
-            int cardType[] = new int[msgsplit.length];
-            int cardID[] = new int[msgsplit.length];
+            int numCards = (msgsplit.length/2);
+            int cardType[] = new int[numCards];
+            int cardID[] = new int[numCards];
             
             int x=0;
             for(int ii=3;ii<msgsplit.length;ii=ii+2,x++){
